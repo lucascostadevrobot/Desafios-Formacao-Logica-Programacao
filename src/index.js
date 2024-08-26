d859b181b6cbdfee06133d43f4440c51c8f3f1cb
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2024-08-26 16:49:28 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+let nomeHeroi = "Lucas da Costa";
+let niveisXP = [10.0000]
+
+for(contador of niveisXP){
+    switch(contador){
+        case 999:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível " + contador);
+    
+        case 1.000:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível " + contador);
+    }      
+}
