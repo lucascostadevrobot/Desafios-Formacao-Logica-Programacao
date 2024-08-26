c55dbcd5e31b084b698139287d67687eab98e6d1
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2024-08-26 16:50:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-let nomeHeroi = "Lucas da Costa";
-let niveisXP = [10.0000]
-
+let nomeHeroi = "Lucas da Costa"
+let niveisXP = [2.000, 2.001]
+if(niveisXP.length > 0){
 for(contador of niveisXP){
     switch(contador){
         case 999:
-            console.log(" O Herói de nome " + nomeHeroi + "está no nível " + contador);
+        case 1.000:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível: " + "Ferro");
+            break;
     
         case 1.000:
-            console.log(" O Herói de nome " + nomeHeroi + "está no nível " + contador);
+        case 2.000:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível: " + "Bronze");
+            break;
+        case 2.001:
+        case 5.000:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível: " + "Prata");
+             break;
+        case 5.001:
+        case 7.000:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível: " + "Ouro");
+             break;
+        case 7.001:
+        case 8.000:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível: " + "Platina");
+             break;
+        case  8.001:
+        case 9.000:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível: " + "Ascendente");
+             break;
+        case 9.001:
+        case 10.000:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível: " + "Imortal");
+              break;
+        case 10.001:
+            console.log(" O Herói de nome " + nomeHeroi + "está no nível: " + "Radiante");
+             break;
+
+        default:
+            console.log(" Mensagem padrão se necessário!");
+
+
     }      
+  }
+}else{
+    console.log("Você precisa iniciar para sair do nivel zero!")
 }
